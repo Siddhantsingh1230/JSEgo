30df0cab95c8bf8497ba23dc2d18cfc2e9f4f071
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-07-26 14:04:51 +0530

    Added RequiredArguments.js

---
+++ Concepts/RequiredArguments.js
@@ -0,0 +1,21 @@
+// In JavaScript, there are no required arguments.
+
+const fxWithArgs = (requiredArg) => {
+  console.log(requiredArg);
+};
+
+fxWithArgs(); // passing no args though it mandatory for my business logic
+// undefined
+// 👆 no error is thrown , but what if  time demands the use requirred argument and if not provided and suitable error must be thrown
+
+// let's try to use the default argument to achieve this scenario
+
+const throwError = () => {
+  throw new Error("Argument is requirred");
+};
+
+const fxWithReqArgs = (requiredArg = throwError()) => {
+  console.log(requiredArg);
+};
+
+fxWithReqArgs(); // Error: Argument is requirred
